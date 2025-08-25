e8f168904a7e0cee08d987942c983b1dc7975d59
Karima <elnadykarima@gmail.com>
2025-08-25 18:37:13 +0300

    create size config class for responsive ui

--- lib/features/splash/presentation/widgets/splash_body.dart
+++ lib/features/splash/presentation/widgets/splash_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_market/core/utils/size_config.dart';
 import 'package:fruits_market/features/onBoarding/presentation/on_boarding_view.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
@@ -40,6 +41,7 @@ class _SplashBodyState extends State<SplashBody>
 
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return Column(
       children: [
         Spacer(),
