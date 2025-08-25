4c56a9ac271a9b00ce835d5071265c6bc93c53e9
Karima <elnadykarima@gmail.com>
2025-08-25 18:53:37 +0300

    add next button to on boarding view body

--- lib/features/onBoarding/presentation/widgets/on_boarding_body.dart
+++ lib/features/onBoarding/presentation/widgets/on_boarding_body.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_market/core/utils/size_config.dart';
+import 'package:fruits_market/core/widgets/custom_buttons.dart';
 
 class OnBoardingBody extends StatelessWidget {
   const OnBoardingBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Stack(children: [
-        
+    return Stack(
+      children: [
+        Positioned(
+          top: SizeConfig.defaultSize! * 10,
+          right: 32,
+          child: Text(
+            'Skip',
+            style: TextStyle(fontSize: 14, color: const Color(0xff898989)),
+            textAlign: TextAlign.left,
+          ),
+        ),
+        Positioned(
+          bottom: SizeConfig.defaultSize! * 10,
+          left: SizeConfig.defaultSize! * 10,
+          right: SizeConfig.defaultSize! * 10,
+          child: CustomGeneralButton(),
+        ),
       ],
     );
   }
