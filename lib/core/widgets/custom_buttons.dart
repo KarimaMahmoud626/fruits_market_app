5d28074ce159cab33cbe2795a57f06f469e6b3db
Karima <elnadykarima@gmail.com>
2025-08-26 22:21:06 +0300

    create custom login with button

--- lib/core/widgets/custom_buttons.dart
+++ lib/core/widgets/custom_buttons.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fruits_market/core/constants.dart';
 import 'package:fruits_market/core/utils/size_config.dart';
+import 'package:fruits_market/core/widgets/space_widget.dart';
 
 class CustomGeneralButton extends StatelessWidget {
   const CustomGeneralButton({super.key, this.text, this.onTap});
@@ -28,6 +29,48 @@ class CustomGeneralButton extends StatelessWidget {
             ),
             textAlign: TextAlign.left,
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomLoginWithButton extends StatelessWidget {
+  const CustomLoginWithButton({
+    super.key,
+    this.icon,
+    required this.text,
+    this.iconColor,
+    this.onTap,
+  });
+
+  final IconData? icon;
+  final String text;
+  final Color? iconColor;
+  final void Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: 60,
+        width: SizeConfig.screenWidth,
+        decoration: BoxDecoration(
+          color: Colors.transparent,
+          border: Border.all(color: Color(0xFF707070)),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(icon, color: iconColor),
+            HorizontalSpace(2),
+            Text(
+              text,
+              style: TextStyle(fontSize: 12, fontWeight: FontWeight.w600),
+            ),
+          ],
         ),
       ),
     );
